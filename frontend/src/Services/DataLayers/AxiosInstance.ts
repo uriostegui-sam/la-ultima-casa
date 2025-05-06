92535916f3b5bc83c7d14c1782689f8b2d48d76a
uriostegui-sam <samyy.uv@gmail.com>
2025-05-06 22:52:09 +0200

    feat: implement Axios instance and base service class for API interaction

---
+++ frontend/src/Services/DataLayers/AxiosInstance.ts
@@ -0,0 +1,45 @@
+import { useAuthStore } from '@/stores/AuthStore';
+import axios from 'axios'
+import type {
+  AxiosInstance,
+  AxiosResponse,
+  InternalAxiosRequestConfig
+} from 'axios'
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: '/api',
+  withCredentials: true,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+axiosInstance.defaults.withXSRFToken = true;
+
+// Request interceptor
+axiosInstance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const authStore = useAuthStore()
+    if (authStore.token) {
+      config.headers.set('Authorization', `Bearer ${authStore.token}`)
+    }
+    return config
+  }
+)
+
+
+// Response interceptor
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse) => response.data,
+  (error) => {
+    if (error.response?.status === 401) {
+      const authStore = useAuthStore()
+      authStore.logout()
+      window.location.href = '/login'
+    }
+    return Promise.reject(error.response?.data || error)
+  }
+)
+
+export default axiosInstance
