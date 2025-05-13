db5d7cad23f07847ae44f9800cc976083c0135cc
uriostegui-sam <samyy.uv@gmail.com>
2025-05-13 22:10:25 +0200

    style: enhance LanguageToggle button hover states

--- frontend/src/components/LanguageToggle.vue
+++ frontend/src/components/LanguageToggle.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { locale, switchLanguage } from '@/Services/Translation/index.ts';
-const current = locale;
+import { locale, switchLanguage } from '@/Services/Translation/index.ts'
+const current = locale
 
 const props = defineProps({
   current: {
@@ -11,20 +11,26 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
-});
+})
 </script>
 
 <template>
   <div class="flex gap-4">
-    <button 
-        :class="{ 'font-bold': current === 'es-ES' }"
-        @click="switchLanguage('es-ES')"
-    >ESP</button>
+    <button
+      class="hover:underline"
+      :class="{ 'font-bold': current === 'es-ES' }"
+      @click="switchLanguage('es-ES')"
+    >
+      ESP
+    </button>
     <span v-if="header" class="text-(--color-salmon)">|</span>
     <span v-else class="text-white">|</span>
     <button
-    :class="{ 'font-bold': current === 'en-GB' }"
+      class="hover:underline"
+      :class="{ 'font-bold': current === 'en-GB' }"
       @click="switchLanguage('en-GB')"
-      >ENG</button>
+    >
+      ENG
+    </button>
   </div>
 </template>
