f6cc06a86e78cde425f1955312528637e9f51039
uriostegui-sam <samyy.uv@gmail.com>
2025-05-14 21:49:31 +0200

    feat: create ActionButton component for reusable CTA buttons

---
+++ frontend/src/components/ActionButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string
+  color?: string
+//   href?: string
+}>()
+
+</script>
+
+<template>
+  <button 
+    class="text-white font-bold py-2 px-4 rounded"
+    :style="{ backgroundColor: `var(${color})` }"
+  >
+    {{props.title}}
+  </button>
+</template>
+
+<style scoped>
+button {
+  background-color: var(--color-teal) !important;
+}
+</style>
