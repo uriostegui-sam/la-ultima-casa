78d958298b5a70b176d80d33694ad30a628c39f9
uriostegui-sam <samyy.uv@gmail.com>
2025-05-14 11:47:46 +0200

    refactor: extract Logo into reusable component and use in Header/Footer

---
+++ frontend/src/components/Logo.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  header?: boolean
+}>();
+
+</script>
+
+<template>
+  <a href="#"
+  :class="header ? '-m-1.5 p-1.5' : ''">
+    <span class="sr-only">La Última Casa</span>
+    <img
+      :class="header ? 'h-20' : 'h-45'"
+      class="w-auto"
+      src="@/assets/Images/logo_UC.svg"
+      alt="Logo de La Última Casa"
+    />
+  </a>
+</template>
