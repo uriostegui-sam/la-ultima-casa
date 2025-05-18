52f57ae63792547438f736b0b03960fb10c5fce1
uriostegui-sam <samyy.uv@gmail.com>
2025-05-18 19:57:27 +0200

    feat(home): add HomeController with complete homepage layout

---
+++ frontend/src/Controllers/Visitors/Home/HomeController.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HeroSection from '@/components/home/HeroSection.vue'
+import TechniquesSection from '@/components/home/TechniquesSection.vue'
+import NewsCarousel from '@/components/layout/NewsCarousel.vue'
+import Header from '@/components/layout/Header.vue'
+import Footer from '@/components/layout/Footer.vue'
+</script>
+
+<template>
+  <Header />
+  <HeroSection :first="true" />
+  <HeroSection :reverse="true" />
+  <HeroSection :reverse="false" />
+  <TechniquesSection />
+  <NewsCarousel />
+  <Footer />
+</template>
+
+<style scoped></style>
