2541deca69a3d4a68fe7587be5911058d4c9eaad
uriostegui-sam <samyy.uv@gmail.com>
2025-05-18 19:51:21 +0200

    feat(button): add filter support to ActionButton

--- frontend/src/components/ActionButton.vue
+++ frontend/src/components/ActionButton.vue
@@ -1,18 +1,40 @@
 <script setup lang="ts">
+import { capitalizeFirstLetter } from '@/Services/Helpers';
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
 const props = defineProps<{
   title?: string
   color?: string
 //   href?: string
+  isFilter?: boolean
+  active?: string
+  filter?: (e: MouseEvent) => void
 }>()
 
+const onClick = (e: MouseEvent) => {
+  if (props.filter) {
+    props.filter(e)
+  }
+}
 </script>
 
 <template>
   <button 
-    class="text-white font-bold py-2 px-4 rounded"
+    class="py-2 px-4"
+    :class="[
+      props.isFilter
+        ? 'rounded-full border text-sm font-medium transition-all border-(--color-salmon)'
+        : 'text-white font-bold rounded hover:bg-white hover:opacity-75',
+      props.isFilter && props.active === props.title
+        ? 'bg-(--color-salmon) text-white'
+        : 'bg-white text-(--color-salmon) hover:bg-(--color-salmon) hover:text-white',
+    ]"
     :style="{ backgroundColor: `var(${color})` }"
+    @click="onClick"
   >
-    <slot>{{props.title}}</slot>
+    <slot>{{capitalizeFirstLetter(t(props.title))}}</slot>
   </button>
 </template>
 
