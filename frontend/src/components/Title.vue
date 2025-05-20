862b342526c691a9b893bdab3dfb1504d90dd7be
uriostegui-sam <samyy.uv@gmail.com>
2025-05-20 21:04:37 +0200

    fix(components): make Title props optional

--- frontend/src/components/Title.vue
+++ frontend/src/components/Title.vue
@@ -5,7 +5,7 @@ import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
 
 const props = defineProps<{
-  title: string
+  title?: string
   hasSubtitle?: boolean
   subtitle?: string
   type?: string
