ed1bbea53b4416a55adbeac27c23fc3b91d513fe
uriostegui-sam <samyy.uv@gmail.com>
2025-05-21 17:11:50 +0200

    chore(imports): update NewsCarousel paths

--- frontend/src/Controllers/Visitors/Home/HomeController.vue
+++ frontend/src/Controllers/Visitors/Home/HomeController.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import HeroSection from '@/components/home/HeroSection.vue'
 import TechniquesSection from '@/components/home/TechniquesSection.vue'
-import NewsCarousel from '@/components/layout/NewsCarousel.vue'
+import NewsCarousel from '@/components/views/Visitors/News/NewsCarousel.vue'
 import Header from '@/components/layout/Header.vue'
 import Footer from '@/components/layout/Footer.vue'
 </script>
