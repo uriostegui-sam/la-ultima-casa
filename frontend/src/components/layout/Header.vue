c639aa3e99415c4c7da8aa38fbb9730f79487b81
uriostegui-sam <samyy.uv@gmail.com>
2025-05-28 18:03:02 +0200

    chore(Header): remove 'contact' from navigation menu

--- frontend/src/components/layout/Header.vue
+++ frontend/src/components/layout/Header.vue
@@ -14,7 +14,6 @@ const menuKeys = [
   { key: 'artists', href: '/artists' },
   { key: 'news', href: '/news' },
   { key: 'workshops', href: '/workshops' },
-  { key: 'contact', href: '/contact' },
 ]
 
 const mobileMenuOpen = ref(false)
