75b95437261081aba705806b74673be3a84f6d2a
uriostegui-sam <samyy.uv@gmail.com>
2025-06-04 11:03:19 +0200

    feat(admin): add initial routing and views for dashboard and artist admin

--- frontend/src/Routers/index.ts
+++ frontend/src/Routers/index.ts
@@ -1,3 +1,4 @@
+import AppLayout from '@/admin/layout/AppLayout.vue'
 import Logo from '@/visitors/components/Logo.vue'
 import ArtistController from '@/visitors/Controllers/Artists/ArtistController.vue'
 import HomeController from '@/visitors/Controllers/Home/HomeController.vue'
@@ -7,6 +8,7 @@ import ArtistInfo from '@/visitors/views/Artists/ArtistInfo.vue'
 import NewsInfo from '@/visitors/views/News/NewsInfo.vue'
 import VisitorLayout from '@/visitors/views/VisitorLayout.vue'
 import WorkshopInfo from '@/visitors/views/Workshop/WorkshopInfo.vue'
+import ArtistAdminController from '@/admin/Controllers/Artists/ArtistAdminController.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -14,12 +16,11 @@ const routes = [
     path: '/',
     component: VisitorLayout,
     children: [
-
       {
         path: '/',
         name: 'Home',
         component: HomeController,
-        meta: { exact: true }
+        meta: { exact: true },
       },
       {
         path: '/artists',
@@ -45,22 +46,38 @@ const routes = [
         path: '/news',
         name: 'News',
         component: NewsController,
-        props: true
+        props: true,
       },
       {
         path: '/news/:id',
         name: 'NewsInfo',
         component: NewsInfo,
-        props: true
+        props: true,
       },
-    ]
+    ],
   },
-//   // 404 fallback
+  {
+    path: '/admin',
+    component: AppLayout,
+    children: [
+      {
+        path: '/admin',
+        name: 'dashboard',
+        component: () => import('@/admin/views/Dashboard.vue'),
+      },
+      {
+        path: '/admin/artists',
+        name: 'adminArtist',
+        component: ArtistAdminController,
+      },
+    ],
+  },
+  //   // 404 fallback
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
     component: Logo,
-  }
+  },
 ]
 
 const router = createRouter({
---
+++ frontend/src/admin/components/dashboard/LatestWorkshopWidget.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { useWorkshopAdminStore } from '@/admin/stores/WorkshopAdminStore';
+import { capitalizeFirstLetter } from '@/shared/services/Helpers';
+import { Languages, locale } from '@/shared/services/Translation';
+import { computed, onMounted, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n()
+const current = locale
+const workshopStore = useWorkshopAdminStore()
+
+const workshopTransformed = computed(() => {
+    return workshopStore.workshops.map((workshop) => ({
+      ...workshop,
+    }))
+})
+
+onMounted(async () => {
+  await workshopStore.getWorkshops()
+});
+</script>
+
+<template>
+    <div class="card">
+        <div class="font-semibold text-xl mb-4">{{ capitalizeFirstLetter(t('latestWorkshops')) }}</div>
+        <DataTable :value="workshopTransformed" :rows="5" :paginator="true" responsiveLayout="scroll">
+            <Column style="width: 15%" :header="capitalizeFirstLetter(t('image'))">
+                <template #body="slotProps">
+                    <img :src="slotProps.data.cover_image_url" :alt="slotProps.data.cover_image_url" width="50" class="shadow" />
+                </template>
+            </Column>
+            <Column field="title" :header="capitalizeFirstLetter(t('title'))" :sortable="true" style="width: 35%">
+                <template #body="slotProps">
+                    {{ current === Languages.English ? slotProps.data.title['en'] : slotProps.data.title['es'] }}
+                </template>
+            </Column>
+            <Column field="type" :header="capitalizeFirstLetter(t('type'))" :sortable="true" style="width: 35%">
+                <template #body="slotProps">
+                    {{ capitalizeFirstLetter(t(slotProps.data.type)) }}
+                </template>
+            </Column>
+            <Column style="width: 15%" :header="capitalizeFirstLetter(t('view'))">
+                <template #body>
+                    <Button icon="pi pi-search" type="button" class="p-button-text"></Button>
+                </template>
+            </Column>
+        </DataTable>
+    </div>
+</template>
