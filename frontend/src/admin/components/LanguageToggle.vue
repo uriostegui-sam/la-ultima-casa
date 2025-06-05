71c5280d063deef7746674a7f48e00340f4a76f6
uriostegui-sam <samyy.uv@gmail.com>
2025-06-05 20:40:23 +0200

    feat(ui): create reusable LanguageToggle component

---
+++ frontend/src/admin/components/LanguageToggle.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { Languages, locale, switchLanguage } from '@/shared/services/Translation'
+const current = locale
+
+defineProps<{
+  header?: boolean
+}>()
+</script>
+
+<template>
+  <div 
+   class="flex gap-4 px-4"
+  >
+    
+    <button
+      class="hover:underline"
+      :class="{ 'font-bold': current === Languages.Spanish }"
+      @click="switchLanguage(Languages.Spanish)"
+    >
+      ESP
+    </button>
+    <button
+      class="hover:underline"
+      :class="{ 'font-bold': current === Languages.English }"
+      @click="switchLanguage(Languages.English)"
+    >
+      ENG
+    </button>
+  </div>
+</template>
+
+<style scoped>
+</style>
