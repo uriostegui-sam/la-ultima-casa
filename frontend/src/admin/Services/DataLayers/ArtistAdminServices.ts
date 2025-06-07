9d6f6ee93c2bff80f447f857fbde46f9ed628fd1
uriostegui-sam <samyy.uv@gmail.com>
2025-06-07 22:23:48 +0200

    refactor(services): update artist form data handling in admin service

--- frontend/src/admin/Services/DataLayers/ArtistAdminServices.ts
+++ frontend/src/admin/Services/DataLayers/ArtistAdminServices.ts
@@ -4,7 +4,7 @@ import axiosInstance from "@/shared/services/DataLayers/AxiosInstance"
 import { BaseService } from "@/shared/services/DataLayers/BaseService"
 import axios from "axios"
 
-class ArtistService extends BaseService {
+class ArtistAdminService extends BaseService {
   constructor() {
     super('/artists')
   }
@@ -38,13 +38,20 @@ class ArtistService extends BaseService {
     const formData = new FormData()
     
     // Append only provided fields
-    if (payload.name) formData.append('name', payload.name) 
     if (payload.user_id) formData.append('user_id', payload.user_id.toString())
-    if (payload.minibio) formData.append('minibio', JSON.stringify(payload.minibio))
-    if (payload.bio) formData.append('bio', JSON.stringify(payload.bio))
-    if (payload.social_links) {
-      formData.append('social_links', JSON.stringify(payload.social_links))
-    }
+    if (payload.user?.email) formData.append('user[email]', payload.user.email.toString())
+    if (payload.user?.name) formData.append('user[name]', payload.user.name.toString())
+    if (payload.user?.lastname) formData.append('user[lastname]', payload.user.lastname.toString())
+    if (payload.minibio?.en) formData.append('minibio[en]', payload.minibio.en.toString())
+    if (payload.minibio?.es) formData.append('minibio[es]', payload.minibio.es.toString())
+    if (payload.minibio?.en) formData.append('minibio[en]', payload.minibio.en.toString())
+    if (payload.bio?.es) formData.append('bio[es]', payload.bio.es.toString())
+    if (payload.bio?.en) formData.append('bio[en]', payload.bio.en.toString())
+    if (payload.social_links?.website) formData.append('social_links[website]', payload.social_links.website.toString())
+    if (payload.social_links?.instagram) formData.append('social_links[instagram]', payload.social_links.instagram.toString())
+    if (payload.social_links?.twitter) formData.append('social_links[twitter]', payload.social_links.twitter.toString())
+    if (payload.social_links?.flickr) formData.append('social_links[flickr]', payload.social_links.flickr.toString())
+    
     if (payload.profile_image) {
       formData.append('profile_image', payload.profile_image)
     }
@@ -52,6 +59,7 @@ class ArtistService extends BaseService {
       payload.skills.forEach(id => formData.append('skills[]', id.toString()))
     }
     
+    console.log(formData)
     formData.append('_method', 'PUT')
 
     const response = await axiosInstance.post<Artist>(
@@ -82,4 +90,4 @@ class ArtistService extends BaseService {
   }
 }
 
-export default new ArtistService()
+export default new ArtistAdminService()
