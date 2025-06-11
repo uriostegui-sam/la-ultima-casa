634eceb9b4a39df28e49a70d5cf72a3ff59fe83d
uriostegui-sam <samyy.uv@gmail.com>
2025-06-11 17:03:01 +0200

    feat(admin): add skills to menu and improve login

---
+++ frontend/src/admin/views/pages/auth/Login.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/shared/stores/AuthStore'
+import { capitalizeFirstLetter } from '@/shared/services/Helpers'
+import { useI18n } from 'vue-i18n'
+import Logo from '@/visitors/components/Logo.vue'
+
+const { t } = useI18n()
+const router = useRouter()
+const credentials = ref({
+  email: '',
+  password: '',
+  rememberMe: false
+})
+const authStore = useAuthStore()
+const loading = ref(false)
+const error = ref('')
+
+async function handleLogin() {
+  try {
+    loading.value = true
+    error.value = ''
+    
+    const response = await authStore.login(credentials.value)    
+    
+    // Redirect based on role
+    if (response.data.user.role === 'admin') {
+      router.push('/admin')
+    } else {
+        // WIP REDIRECT A LA PÁGINA PERSONAL DEL ARTISTA Y SOLO PERMITIR QUE VAYA A SUS OBRAS
+    //   router.push('/dashboard')
+    }
+    
+  } catch (err: any) {
+    error.value = err.message || 'Invalid credentials'
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
+<template>
+    <FloatingConfigurator />
+    <div class="flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden">
+        <div class="flex flex-col items-center justify-center">
+            <div class="bg-(--color-light-salmon) rounded-4xl">
+                <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20">
+                    <div class="text-center mb-8 flex flex-col items-center ">
+                        <Logo class="pb-8"/>
+                        <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">{{ capitalizeFirstLetter(t('welcomeTo')) }} La Última Casa Admin</div>
+                        <span class="text-muted-color font-medium">{{ capitalizeFirstLetter(t('signInCont')) }}</span>
+                    </div>
+
+                    <!-- Error message display -->
+                    <div v-if="error" class="mb-4 p-4 bg-red-100 text-red-700 rounded-lg">
+                        {{ error }}
+                    </div>
+
+                    <div>
+                        <label for="email1" class="block text-surface-900 dark:text-surface-0 text-xl font-medium mb-2">{{ capitalizeFirstLetter(t('email')) }}</label>
+                        <InputText 
+                            id="email1" 
+                            type="email" 
+                            :placeholder="capitalizeFirstLetter(t('email'))"
+                            class="w-full mb-8" 
+                            v-model="credentials.email"
+                            :disabled="loading"
+                        />
+
+                        <label for="password1" class="block text-surface-900 dark:text-surface-0 font-medium text-xl mb-2">{{ capitalizeFirstLetter(t('password')) }}</label>
+                        <Password 
+                            id="password1" 
+                            v-model="credentials.password" 
+                            :placeholder="capitalizeFirstLetter(t('password'))"
+                            :toggleMask="true" 
+                            class="mb-4" 
+                            fluid 
+                            :feedback="false"
+                            :disabled="loading"
+                        ></Password>
+
+                        <div class="flex items-center justify-between mt-2 mb-8 gap-8">
+                            <div class="flex items-center">
+                                <Checkbox 
+                                    v-model="credentials.rememberMe" 
+                                    id="rememberme1" 
+                                    binary 
+                                    class="mr-2"
+                                    :disabled="loading"
+                                ></Checkbox>
+                                <label for="rememberme1">{{ capitalizeFirstLetter(t('rememberMe')) }}</label>
+                            </div>
+                            <!-- WIP FORGOT PASSWORD -->
+                            <!-- <span class="font-medium no-underline ml-2 text-right cursor-pointer text-primary">{{ capitalizeFirstLetter(t('forgotPassword')) }}</span> -->
+                        </div>
+                        
+                        <Button 
+                            :label="capitalizeFirstLetter(t('signIn'))" 
+                            severity="warn"
+                            class="w-full bg-salmon"
+                            @click="handleLogin"
+                            :loading="loading"
+                            :disabled="loading"
+                          />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
