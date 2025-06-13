9bc1485a763551c13d648892ef4f06736f159b70
uriostegui-sam <samyy.uv@gmail.com>
2025-06-13 19:31:29 +0200

    feat(admin-about): create AboutUs model and basic CRUD

---
+++ backend/app/Http/Requests/StoreAboutUsRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAboutUsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user() && $this->user()->isAdmin();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'number' => 'required|numeric',
+            'cover_image' => 'required|image|max:2048',
+            'mail' => 'required|email',
+            // 'address' => 'required|array',
+            'address.text' => 'required|string',
+            'address.map' => 'required|string'
+        ];
+    }
+}
