b4ece0bcb0a861633539f331630734ac8012af6c
uriostegui-sam <samyy.uv@gmail.com>
2025-06-18 19:39:31 +0200

    fix(visitor): only show news carousel when there are enough items

--- frontend/src/visitors/views/News/NewsCarousel.vue
+++ frontend/src/visitors/views/News/NewsCarousel.vue
@@ -78,7 +78,9 @@ onMounted(() => {
 </script>
 
 <template>
-  <section class="relative w-full max-w-7xl embla my-8 mx-auto ps-5">
+  <section 
+    v-if="newsTransformed.length > 3"
+    class="relative w-full max-w-7xl embla my-8 mx-auto ps-5">
     <div class="flex justify-between items-center py-3 xl:px-0 pe-5">
         <h1 class="text-(--color-teal)">{{ capitalizeFirstLetter($t('latestNews')) }}</h1>
         <ActionButton
@@ -121,6 +123,7 @@ onMounted(() => {
         />
       </div>
   </section>
+  <section v-else ></section>
 </template>
 
 <style scoped>
