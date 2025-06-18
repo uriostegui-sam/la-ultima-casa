5c6f52986132f1695e70e209b9a039cb1398a2e1
uriostegui-sam <samyy.uv@gmail.com>
2025-06-18 19:32:02 +0200

    feat: redesign dashboard with count widgets and loading state

---
+++ frontend/src/admin/components/dashboard/DashboadCountWidget.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+  icon?: string
+  count: number
+}>();
+</script>
+
+<template>
+    <div class="col-span-12 lg:col-span-6 xl:col-span-3">
+        <div class="card mb-0">
+            <div class="flex justify-between mb-4">
+                <div>
+                    <span class="block text-muted-color font-medium mb-4">{{ props.title }}</span>
+                    <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">{{ props.count }}</div>
+                </div>
+                <div class="flex items-center justify-center bg-cyan-100 dark:bg-cyan-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
+                    <i :class="`pi ${props.icon} text-cyan-500 !text-xl`"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- frontend/src/admin/views/Dashboard.vue
+++ frontend/src/admin/views/Dashboard.vue
@@ -1,17 +1,49 @@
 <script setup>
 import LatestWorkshopWidget from '@/admin/components/dashboard/LatestWorkshopWidget.vue';
+import DashboadCountWidget from '../components/dashboard/DashboadCountWidget.vue';
+import { capitalizeFirstLetter } from '@/shared/services/Helpers';
+import { useI18n } from 'vue-i18n';
+import { useAdminArtistStore } from '../stores/ArtistAdminStore';
+import { useAdminArtworkStore } from '../stores/ArtworkAdminStore';
+import { useAdminWorkshopStore } from '../stores/WorkshopAdminStore';
+import { onMounted, ref } from 'vue';
+import LoadingComponent from '@/shared/components/LoadingComponent.vue';
+
+const { t } = useI18n();
+const artistAdminStore = useAdminArtistStore();
+const artworkAdminStore = useAdminArtworkStore();
+const workshopAdminStore = useAdminWorkshopStore();
+const artists = ref(0);
+const artwork = ref(0);
+const workshops = ref(0);
+
+const loading = ref(true);
+
+onMounted(async () => {
+    await artistAdminStore.getArtists();
+    await artworkAdminStore.getArtworks();
+    await workshopAdminStore.getWorkshops();
+
+    artists.value = artistAdminStore.artists.length;
+    artwork.value = artworkAdminStore.artworks.length;
+    workshops.value = workshopAdminStore.workshops.length;
+
+    loading.value = false;
+});
 </script>
 
 <template>
-    <div class="grid grid-cols-12 gap-8">
+    <div v-if="!loading"
+        class="grid grid-cols-12 gap-8">
 
-        <div class="col-span-12 xl:col-span-6">
-            <LatestWorkshopWidget />
-            <LatestWorkshopWidget />
+        <div class="col-span-12 flex justify-between">
+            <DashboadCountWidget icon="pi-palette" :title="capitalizeFirstLetter(t('artists'))" :count="artists"/>
+            <DashboadCountWidget icon="pi-pen-to-square" :title="capitalizeFirstLetter(t('artwork'))" :count="artwork"/>
+            <DashboadCountWidget icon="pi-wrench" :title="capitalizeFirstLetter(t('workshops'))" :count="workshops"/>
         </div>
-        <div class="col-span-12 xl:col-span-6">
-            <LatestWorkshopWidget />
+        <div class="col-span-12">
             <LatestWorkshopWidget />
         </div>
     </div>
+    <div v-else><LoadingComponent /></div>
 </template>
