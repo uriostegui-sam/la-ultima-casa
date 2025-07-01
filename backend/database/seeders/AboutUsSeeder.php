d2f093f410c2e68e11b08b8b2f2f3e5b1c6814a6
uriostegui-sam <samyy.uv@gmail.com>
2025-07-01 14:46:48 +0000

    feat(seed): update AboutUs and Artist seeders for production

--- backend/database/seeders/AboutUsSeeder.php
+++ backend/database/seeders/AboutUsSeeder.php
@@ -13,8 +13,20 @@ class AboutUsSeeder extends Seeder
      */
     public function run(): void
     {
-        AboutUs::factory()
-            ->count(1)
-            ->create();
+	AboutUs::create([
+            'number' => '044 312 119 5692',
+            'cover_image' => 'about_us/images/cover-image.jpg',
+            'mail' => 'carlos.perez@gmail.com',
+            'address' => [
+                'text' => 'Palma Kerpis 152,</br>
+                Colinas de Santa Barbara, Sta Bárbara,</br>
+                28017 Colima, Col., México',
+                'map' => '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15065.757142672133!2d-103.72817832131042!3d19>'
+            ],
+            'description' => [
+                'en' => 'It is an art workshop focused on providing <strong>enriching artistic experiences</strong> for people of all ages>',
+                'es' => 'Es un taller de arte enfocado en brindar <strong>experiencias artísticas enriquecedoras</strong> para personas de>'
+            ],
+        ]);
     }
 }
