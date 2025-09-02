da7ccde3f60dd7aba61301f4d4885c42ef7c7a9e
uriostegui-sam <samyy.uv@gmail.com>
2025-09-02 21:42:59 +0200

    feat(admin-aboutUs): add logo to About Us model

--- backend/app/Models/AboutUs.php
+++ backend/app/Models/AboutUs.php
@@ -9,12 +9,13 @@ class AboutUs extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['cover_image', 'number', 'mail', 'address', 'description', 'logo'];
+    protected $fillable = ['cover_image', 'number', 'mail', 'address', 'description', 'logo_header', 'logo_footer'];
 
     protected $casts = [
         'cover_image' => 'string',
         'address' => 'array',
         'description' => 'array',
-        'logo' => 'string',
+        'logo_header' => 'string',
+        'logo_footer' => 'string',
     ];
 }
