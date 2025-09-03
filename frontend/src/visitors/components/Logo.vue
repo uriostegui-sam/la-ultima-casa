39fb7d409d2623ca84422de1416ff39df5e27266
uriostegui-sam <samyy.uv@gmail.com>
2025-09-03 11:25:07 +0200

    css(logo): change logo sizes

--- frontend/src/visitors/components/Logo.vue
+++ frontend/src/visitors/components/Logo.vue
@@ -28,8 +28,7 @@ onMounted(async () => {
       <img
         v-if="aboutUs"
         :class="[
-          header ? 'w-20 md:w-25' : hero ? 'w-40 lg:w-55' : 'w-25 md:w-35',
-          hero ? 'opacity-50' : '',
+          header ? 'w-10 md:w-20' : hero ? '' : 'w-25 md:w-35',
         ]"
         :src="`${baseUrl}/${header ? `${aboutUs.logo_header}` : hero ? `aboutUs/logo/logo-hero.png` : `${aboutUs.logo_footer}`}`"
         alt="logo de la última casa"
